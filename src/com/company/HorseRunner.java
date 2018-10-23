794e8d2925d6fea6384c1211ab2473b9d43f7be3
rshek8965 <rshek8965@bths.edu>
2018-10-23 11:44:05 -0400

    added class

--- src/com/company/HorseRunner.java
+++ src/com/company/HorseRunner.java
@@ -12,7 +12,7 @@ public class HorseRunner {
         horses[1] = horse1;
         horses[3] = horse2;
         horses[5] = horse3;
-        HorseBarn = new HorseBarn(horses);
+        HorseBarn barn = new HorseBarn(horses);
 
         System.out.println(barn);
         System.out.println(" Dusty Trail is in space: " + barn.findHorseSpace("Dusty Trail"));
---
+++ src/com/company/Zebra.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Zebra implements Horse{
+
+    private String name;
+    private int weight;
+
+    
