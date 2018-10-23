a2b0e8147a8e2d906c994caa2119bdf67f4a1bf6
rshek8965 <rshek8965@bths.edu>
2018-10-23 11:46:47 -0400

    edited class

--- src/com/company/Zebra.java
+++ src/com/company/Zebra.java
@@ -1,8 +1,14 @@
 package com.company;
 
-public class Zebra implements Horse{
+public class Zebra implements Horse {
 
     private String name;
     private int weight;
 
-    
+    public Zebra (String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    public String getname() {return this.name;}
+    public int getWeight() {return this.weight;}
+}
