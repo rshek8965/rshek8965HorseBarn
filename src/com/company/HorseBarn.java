29b5c1712c74288c22bd1b11a0b1988413bf5ab2
rshek8965 <43213249+rshek8965@users.noreply.github.com>
2018-10-24 13:05:15 -0400

    Update HorseBarn.java
    added constructor

--- src/com/company/HorseBarn.java
+++ src/com/company/HorseBarn.java
@@ -4,6 +4,10 @@ public class HorseBarn {
 
     private Horse[] spaces;
 
+    public HorseBarn (Horse[] spaces) {
+            this.spaces = spaces;
+    }
+
     public int findHorseSpace(String name) {
         for (int i = 0; i < this.spaces.length; i++) {
             if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
